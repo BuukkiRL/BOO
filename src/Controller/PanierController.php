bed9a70510b4f7ad2828713cb8aed388cbbf9bf1
Buukki <romainlf28@gmail.com>
2022-11-17 16:11:29 +0100

    ajout lien easy admin

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PanierController extends AbstractController
@@ -14,5 +16,22 @@ class PanierController extends AbstractController
         return $this->render('panier/index.html.twig', [
             'controller_name' => 'PanierController',
         ]);
+    }
+
+    #[Route('/add/{slug}', name: 'add_panier')]
+    public function add(Product $product, SessionInterface $session){
+
+        $panier = $session->get("panier", []);
+        $slug = $product->getSlug();
+
+        if(!empty($panier[$slug])){
+            $panier[$slug]++;
+        }else{
+            $panier[$slug] = 1;
+        }
+
+        $session->set("panier", $panier);
+
+        return $this->redirectToRoute('app_produit');
     }
 }
